5ebbc7510281d56149b215be04c2d95352b018b4
penguinfreud <qiyiwsy@yahoo.com>
2016-12-11 14:39:37 +0800

    some automation

--- ZFC.v
+++ ZFC.v
@@ -115,6 +115,19 @@ Proof.
       contradiction.
 Qed.
 
+Lemma nonempty_neg_iff: forall x, ~nonempty x <-> x = empty_set.
+Proof.
+  split; intros.
+  - apply extensionality.
+    split; intros.
+    + assert (nonempty x).
+      { exists u. apply H0. }
+      apply H in H1. inversion H1.
+    + apply empty_set_no_elem in H0. inversion H0.
+  - unfold not. intros. rewrite H in H0.
+    destruct H0. apply empty_set_no_elem in H0. inversion H0.
+Qed.
+
 Lemma empty_subset: forall x, empty_set :s x.
 Proof.
   unfold subset. intros.
@@ -187,10 +200,22 @@ Proof.
 Qed.
 
 
+Create HintDb zfc.
+
+Lemma set_eq_iff: forall x y, x=y <-> (forall u, u :i x <-> u :i y).
+Proof.
+  split; intros.
+  - rewrite H. reflexivity.
+  - apply extensionality. apply H.
+Qed.
+
+Hint Rewrite set_eq_iff: zfc.
+
 Definition biunion x y := union (pair x y).
 Definition join x y := sep (fun u => u :i y) x.
 Definition diff x y := sep (fun x => ~x :i y) x.
 Notation "x -- y" := (diff x y) (at level 56, left associativity).
+Definition symm_diff x y := biunion (x -- y) (y -- x).
 
 Lemma biunion_iff: forall x y u,
   u :i biunion x y <-> u :i x \/ u :i y.
@@ -208,6 +233,8 @@ Proof.
     [left|right]; trivial.
 Qed.
 
+Hint Rewrite biunion_iff: zfc.
+
 Lemma join_iff: forall x y u,
   u :i join x y <-> u :i x /\ u :i y.
 Proof.
@@ -217,6 +244,8 @@ Proof.
   destruct H; split; trivial.
 Qed.
 
+Hint Rewrite join_iff: zfc.
+
 Lemma diff_iff: forall x y u, u :i x -- y <->
   u :i x /\ ~u :i y.
 Proof.
@@ -225,123 +254,266 @@ Proof.
   - apply sep_exists. apply H.
 Qed.
 
-Ltac set_oper :=
-  repeat unfold subset; intros; match goal with
-    | [_:_ |- _ = _] => try (apply extensionality; split; intros)
-    | [H: _ = _ |- _] => try (apply extensionality in H; destruct H)
-    | [_:_ |- ~_ :i empty_set] => apply empty_set_no_elem
-    | [H: _ :i empty_set |- _] => apply empty_set_no_elem in H; inversion H
-    | [_:_ |- _ :i biunion _ _] => rewrite ? biunion_iff, ? join_iff, ? diff_iff
-    | [H: _ :i biunion _ _ |- _] => apply biunion_iff in H; destruct H
-    | [_:_ |- _ :i join _ _] => apply join_iff; split
-    | [H: _ :i join _ _ |- _] => apply join_iff in H; destruct H
-    | [_:_ |- _ :i diff _ _] => apply diff_iff; split
-    | [H: _ :i diff _ _ |- _] => apply diff_iff in H; destruct H
+Hint Rewrite diff_iff: zfc.
+
+Lemma symm_diff_iff: forall x y u, u :i symm_diff x y <->
+  (u :i x /\ ~u :i y) \/ (u :i y /\ ~u :i x).
+Proof.
+  unfold symm_diff; split; intros;
+  autorewrite with zfc in *; auto.
+Qed.
+
+Hint Rewrite symm_diff_iff: zfc.
+
+Ltac autoelim :=
+  intros; repeat match goal with
+    | [_:_ |- _ /\ _] => split; intros
+    | [_:_ |- _ <-> _] => split; intros
+    | [H: _ \/ _ |- _] => destruct H
+    | [H: _ /\ _ |- _] => destruct H
+    | [H: False |- _] => inversion H
   end.
 
-Ltac set_rewrite :=
-  repeat rewrite ? biunion_iff, ? join_iff, ? diff_iff in *.
+Ltac set_oper :=
+  repeat (autorewrite with zfc in *;
+    unfold subset; autoelim;
+    repeat match goal with
+      | [H: _ :i empty_set |- _] => apply empty_set_no_elem in H
+      | [_:_ |- ~ _ :i empty_set] => apply empty_set_no_elem
+    end; auto).
 
-Lemma biunion_subset_l: forall x y, x :s biunion x y.
+Lemma not_or_iff: excluded_middle ->
+  forall A B: Prop, ~(A \/ B) <-> ~A /\ ~B.
 Proof.
-  set_oper; auto.
+  intros EM A B. split; intros.
+  - destruct (EM A).
+    + destruct (H (or_introl H0)).
+    + split; trivial. destruct (EM B).
+      * destruct (H (or_intror H1)).
+      * apply H1.
+  - unfold not. intros. destruct H, H0; contradiction.
+Qed.
+
+Lemma not_and_iff: excluded_middle ->
+  forall A B: Prop, ~(A /\ B) <-> ~A \/ ~B.
+Proof.
+  intro EM. split; intros.
+  - destruct (EM A), (EM B).
+    + destruct (H (conj H0 H1)).
+    + right. trivial.
+    + left. trivial.
+    + left. trivial.
+  - unfold not. intros [HA HB].
+    destruct H; contradiction.
+Qed.
+
+Lemma not_imply_iff: excluded_middle ->
+  forall A B: Prop, ~(A -> B) -> A /\ ~B.
+Proof.
+  intro EM. split; intros.
+  - destruct (EM A); trivial.
+    assert (A -> B).
+    { intro. destruct (H0 H1). }
+    destruct (H H1).
+  - destruct (EM B).
+    + assert (A -> B).
+      { intro. trivial. }
+      destruct (H H1).
+    + trivial.
+Qed.
+
+Lemma double_neg: excluded_middle -> forall A: Prop, ~~A <-> A.
+Proof.
+  intro EM.
+  split; intros.
+  - destruct (EM A); trivial.
+    destruct (H H0).
+  - unfold not. intros. destruct (H0 H).
+Qed.
+
+Ltac classic :=
+  repeat (autoelim; match goal with
+    | [EM: excluded_middle, H: ~ (_ /\ _) |- _] => rewrite (not_and_iff EM) in H
+    | [EM: excluded_middle, H: ~ (_ \/ _) |- _] => rewrite (not_or_iff EM) in H
+    | [EM: excluded_middle, H: ~ (_ -> _) |- _] => rewrite (not_imply_iff EM) in H
+    | [EM: excluded_middle, H: ~~ _ |- _] => rewrite (double_neg EM) in H
+    | [EM: excluded_middle |- ~ (_ /\ _)] => rewrite (not_and_iff EM)
+    | [EM: excluded_middle |- ~ (_ \/ _)] => rewrite (not_or_iff EM)
+    | [EM: excluded_middle |- ~ (_ -> _)] => rewrite (not_imply_iff EM)
+    | [EM: excluded_middle |- ~~ _] => rewrite (double_neg EM)
+  end); autoelim.
+
+Lemma biunion_subset_l: forall x y, x :s biunion x y. 
+Proof.
+  set_oper.
 Qed.
 
 Lemma biunion_comm: forall x y, biunion x y = biunion y x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma biunion_subset_r: forall x y, y :s biunion x y.
 Proof.
-  intros. rewrite biunion_comm. apply biunion_subset_l.
+  set_oper.
 Qed.
 
 Lemma biuniona_assoc: forall x y z,
   biunion (biunion x y) z = biunion x (biunion y z).
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_comm: forall x y, join x y = join y x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_subset_l: forall x y, join x y :s x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_subset_r: forall x y, join x y :s y.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_assoc: forall x y z, join (join x y) z = join x (join y z).
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_biunion_dist: forall x y z,
   join x (biunion y z) = biunion (join x y) (join x z).
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_empty_set: forall x, join empty_set x = empty_set.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma join_idempotent: forall x, join x x = x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 
 Lemma biunion_empty_set: forall x, biunion empty_set x = x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma biunion_join_dist: forall x y z,
   biunion x (join y z) = join (biunion x y) (biunion x z).
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 
 Lemma empty_sub_x: forall x, empty_set -- x = empty_set.
 Proof.
-  intros.
-  apply sep_empty_set.
+  set_oper.
 Qed.
 
 Lemma x_sub_empty: forall x, x -- empty_set = x.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
 Lemma diff_join_assoc: forall x y z, join x (y--z) = join x y -- z.
 Proof.
-  repeat (set_oper; auto).
+  set_oper.
 Qed.
 
-Lemma diff_join_biunion_assoc: excluded_middle ->
+Lemma demorgan_join: excluded_middle ->
   forall x y z, x -- (join y z) = biunion (x -- y) (x -- z).
 Proof.
-  repeat set_oper; auto; unfold not in *; set_rewrite; intros; auto.
-  - assert (u :i y \/ ~u :i y). apply H.
-    destruct H2.
-    + assert (u :i y -> ~u :i z).
-      { unfold not. intros. apply H1. split; trivial. }
-      apply H3 in H2.
-      right. split; trivial.
-    + left. split; trivial.
-  - destruct H2. contradiction.
-  - destruct H2. contradiction.
+  intro EM.
+  repeat (set_oper; classic; auto).
 Qed.
+
+Lemma demorgan_biunion: forall x y z,
+  x -- (biunion y z) = join (x -- y) (x -- z).
+Proof.
+  set_oper.
+  unfold not. intros. destruct H3; contradiction.
+Qed.
+
+Lemma diff_subset: excluded_middle -> forall x y,
+  x -- y = empty_set <-> x :s y.
+Proof.
+  intro EM.
+  split; set_oper.
+  - assert (~u :i empty_set). apply empty_set_no_elem.
+    rewrite <- H in H1. rewrite diff_iff in H1.
+    assert (u :i y \/ ~u :i y). apply EM.
+    destruct H2.
+    + apply H2.
+    + destruct (H1 (conj H0 H2)).
+  - apply H in H0. contradiction.
+Qed.
+
+Lemma symm_diff_comm: forall x y, symm_diff x y = symm_diff y x.
+Proof.
+  set_oper.
+Qed.
+
+Lemma symm_diff_assoc: excluded_middle -> forall x y z,
+  symm_diff (symm_diff x y) z = symm_diff x (symm_diff y z).
+Proof.
+  intro EM.
+  set_oper; classic; classic;
+  [left | right | | | left | left | | | right | right];
+  try contradiction; split; intros;
+  classic; auto.
+Qed.
+
+Lemma subset_iff_join: forall x y, x :s y <-> join x y = x.
+Proof.
+  set_oper. apply H in H0. set_oper.
+Qed.
+
+Lemma subset_iff_biunion: forall x y, x :s y <-> biunion x y = y.
+Proof.
+  set_oper. apply H. set_oper.
+Qed.
+
+Lemma biunion_diff: forall x y, (biunion x y) -- y = x -- y.
+Proof.
+  set_oper. contradiction.
+Qed.
+
+Lemma diff_join: forall x y, x -- (join x y) = x -- y.
+Proof.
+  set_oper. unfold not. intros. destruct (H0 (proj2 H1)).
+Qed.
+
+Lemma diff_diff_join: excluded_middle ->
+  forall x y, x -- (x -- y) = join x y.
+Proof.
+  set_oper; classic; try contradiction; auto; classic; auto.
+Qed.
+
+Lemma diff_x_diff_y_z__biunion_diff_x_y_join_x_z:
+  excluded_middle -> forall x y z,
+    x -- (y -- z) = biunion (x -- y) (join x z).
+Proof.
+  set_oper; classic; auto.
+  rewrite (double_neg H) in H1.
+  right. split; trivial.
+Qed.
+
+Definition arbitrary_join x y := sep (fun x => forall u, u :i y -> x :i u) x.
+
+Lemma arbitrary_join_subset_l: forall x y u, u :i y -> arbitrary_join x y :s u.
+Proof.
+  unfold arbitrary_join, subset. intros.
+  apply sep_prop in H0. apply H0. apply H.
+Qed.
+
 
 Definition is_power_set x y := forall u, u :i y <-> u :s x.
 Axiom Power_set: forall x, exists y, is_power_set x y.
@@ -366,6 +538,15 @@ Proof.
   intros. apply power_set_exists. apply subset_refl.
 Qed.
 
+Lemma power_set_iff: forall x u, u :i power_set x <-> u :s x.
+Proof.
+  split; intros.
+  - apply power_set_exists in H. apply H.
+  - apply power_set_exists. apply H.
+Qed.
+
+Hint Rewrite power_set_iff: zfc.
+
 
 Definition singleton x := pair x x.
 Definition succ x := union (pair x (singleton x)).
@@ -376,6 +557,8 @@ Proof.
   - apply pair_exists in H. destruct H; apply H.
   - apply pair_exists. left. apply H.
 Qed.
+
+Hint Rewrite singleton_iff: zfc.
 
 Lemma succ_iff: forall x u, u :i succ x <-> u = x \/ u :i x.
 Proof.
@@ -394,6 +577,8 @@ Proof.
       * apply pair_exists. left. reflexivity.
       * apply H.
 Qed.
+
+Hint Rewrite succ_iff: zfc.
 
 
 Definition induction_set x :=
@@ -422,6 +607,13 @@ Proof.
   assert (nonempty (join (singleton x) x)).
   exists x. apply H1.
   apply nonempty_correct in H2. contradiction.
+Qed.
+
+Lemma power_set_not_subset: forall x, ~power_set x :s x.
+Proof.
+  unfold not. set_oper.
+  assert (x :i power_set x). apply power_set_x_has_x.
+  apply H in H0. apply no_self_ref in H0. trivial.
 Qed.
 
 
@@ -495,6 +687,8 @@ Proof.
     apply pair_comm.
 Qed.
 
+Hint Rewrite pair_eq_iff: zfc.
+
 Lemma ordered_pair_injective: forall x1 x2 y1 y2,
   x1,,y1 = x2,,y2 <-> x1 = x2 /\ y1 = y2.
 Proof.
@@ -529,7 +723,7 @@ Definition cast_fun R x := descr (fun y => R x y).
 Definition uncast_fun X f x y := x :i X /\ f x = y.
 
 Definition reflexive X R := forall x, x :i X -> R x x.
-Definition irreflexive X R := forall x, x :i X -> ~R x x.
+Definition irreflexive R := forall x, ~R x x.
 Definition symmetric R := forall x y, R x y -> R y x.
 Definition asymmetric R := forall x y, R x y -> ~R y x.
 Definition antisymmetric R := forall x y, R x y -> R y x -> x = y.
@@ -537,10 +731,10 @@ Definition transitive R := forall x y z, R x y -> R y z -> R x z.
 Definition antitransitive R := forall x y z, R x y -> R y z -> ~R x z.
 Definition trichotomous R := forall x y, R x y \/ R y x \/ x=y.
 
-Definition partial_order X R := reflexive X R /\ antisymmetric R.
-Definition strict_partial_order X R := irreflexive X R /\ antitransitive R.
-Definition total_order X R := partial_order X R /\ trichotomous R.
-Definition strict_total_order X R := strict_partial_order X R /\ trichotomous R.
+Definition partial_order R := antisymmetric R /\ transitive R.
+Definition strict_partial_order R := irreflexive R /\ transitive R.
+Definition total_order R := partial_order R /\ trichotomous R.
+Definition strict_total_order R := strict_partial_order R /\ trichotomous R.
 
 Definition equivalence_relation X R := reflexive X R /\ symmetric R /\ transitive R.
 Definition partition X p := mutual_disjoint p /\ union p = X.
@@ -566,9 +760,27 @@ Definition upper_bound X R S a := a :i X /\ S :s X /\ forall x, x :i S -> R x a.
 Definition inf X R S a := lower_bound X R S a /\ forall b, lower_bound X R S b -> R b a.
 Definition sup X R S a := upper_bound X R S a /\ forall b, upper_bound X R S b -> R a b.
 
-Definition minimal_elem_prop X R := forall S, S :s X -> exists a, minimal_elem S R a.
-Definition well_order X R := total_order X R /\ minimal_elem_prop X R.
-Definition strict_well_order X R := strict_total_order X R /\ minimal_elem_prop X R.
+Definition minimal_elem_prop X R := forall S,
+  S :s X -> nonempty S -> exists a, minimal_elem S R a.
+Definition well_order X R := total_order R /\ minimal_elem_prop X R.
+Definition strict_well_order X R := strict_total_order R /\ minimal_elem_prop X R.
+
+Definition refl_closure R x y := x = y \/ R x y.
+
+Lemma swo_subset: forall X Y R, strict_well_order X R ->
+  Y :s X -> strict_well_order Y R.
+Proof.
+  unfold strict_well_order, minimal_elem_prop. intros.
+  split.
+  - apply H.
+  - intros.
+    apply (subset_trans S Y X) in H1.
+    apply H in H1. destruct H1.
+    exists x. apply H1.
+    assumption. assumption.
+Qed.
+
+
 
 Definition injective R := forall x y z, R x z -> R y z -> x = y.
 Definition surjective r Y := ran r = Y.
@@ -633,9 +845,9 @@ Proof.
   apply succ_nat, O_is_nat.
 Qed.
 
-Definition initial_segment X R I := total_order X R /\ I :s X /\
+Definition initial_segment X R I := total_order R /\ I :s X /\
   forall x y, x :i I -> y :i X -> R y x -> y :i I.
-Definition strict_initial_segment X R I := strict_total_order X R /\ I :s X /\
+Definition strict_initial_segment X R I := strict_total_order R /\ I :s X /\
   forall x y, x :i I -> y :i X -> R y x -> y :i I.
 
 Definition proper_initial_segment X R I := initial_segment X R I /\ I :ps X.
@@ -645,7 +857,74 @@ Definition strict_proper_initial_segment X R I
 Definition transitive_set X := forall x, x :i X -> x :s X.
 Definition ordinal_number x := transitive_set x /\ strict_well_order x In.
 
-Lemma wo_init_seg_sup: forall W R S, strict_well_order W R ->
-  strict_proper_initial_segment W R S ->
-  exists a, a :i W /\ S = sep (fun x => R x a) W.
+Definition maximal_elem_prop X R := forall S,
+  S :s X -> nonempty S -> exists a, maximal_elem S R a.
 
+Definition inf_prop X R := forall S,
+  S :s X -> (exists a, lower_bound X R S a) -> exists a, inf X R S a.
+Definition sup_prop X R := forall S,
+  S :s X -> (exists a, upper_bound X R S a) -> exists a, sup X R S a.
+
+Lemma ordinal_elem: forall x y,
+  ordinal_number x -> y :i x -> ordinal_number y.
+Proof.
+  unfold ordinal_number, transitive_set. intros.
+  destruct H.
+  split.
+  - unfold subset. intros.
+    apply ((proj2 (proj1 (proj1 H1))) u x0 y); auto.
+  - apply swo_subset with x; auto.
+Qed.
+
+Lemma wo_init_seg_sup: excluded_middle -> forall W R S,
+  strict_well_order W R ->
+    strict_proper_initial_segment W R S ->
+      exists a, a :i W /\ S = sep (fun x => R x a) W.
+Proof.
+  intros EM W R S. intros.
+  assert (W -- S :s W).
+  { set_oper. }
+  destruct H. apply H2 in H1 as H3.
+  destruct H0. destruct H4. destruct H5.
+  assert (nonempty (W -- S)).
+  { exists x. apply diff_iff. apply H5. }
+  apply H3 in H6. destruct H6.
+  exists x0. split.
+  - apply H1. apply H6.
+  - apply extensionality. split; intros.
+    + apply sep_exists. split.
+      * apply H0. apply H7.
+      * destruct (proj2 H) with u x0 as [H8|H8].
+        -- apply H8.
+        -- assert (x0 :i W -- S). apply H6.
+           apply diff_iff in H9. destruct H9.
+           destruct H8.
+           ++ assert (x0 :i S).
+              { apply (proj2 (proj2 H0)) with u; auto. }
+              contradiction.
+           ++ rewrite H8 in H7. contradiction.
+    + destruct (EM (u :i S)).
+      * apply H8.
+      * assert (u :i W -- S).
+        { apply diff_iff. split.
+          - apply sep_subset in H7. apply H7.
+          - apply H8. }
+        apply H6 in H9.
+        destruct H9.
+        -- apply sep_prop in H7. rewrite H9 in H7.
+           apply H in H7. inversion H7.
+        -- apply sep_prop in H7 as H10.
+           apply ((proj2 (proj1 H)) u x0 u) in H10.
+           apply H in H10. inversion H10.
+           apply sep_subset in H7.
+           apply H9.
+Qed.
+
+Definition strict_increasing_function R f := strict_total_order R /\
+  forall x y, R x y -> R (f x) (f y).
+
+Lemma wo_strict_inc_func__x_lt_fx: forall W R f, strict_well_order W R ->
+  strict_increasing_function R f ->
+    forall x, x :i W -> R x (f x) \/ x = f x.
+Proof.
+  Admitted.
